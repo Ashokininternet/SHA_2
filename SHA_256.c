0941cd5d1a140919bdc33b1e67d860949b542492
Ashok S.R <ashok.106sr@gmail.com>
2026-06-27 22:48:57 +0530

    fix some spaces and stuff

--- SHA_256.c
+++ SHA_256.c
@@ -6,13 +6,17 @@ int main(int argc, char *argv[]) {
   char value[256];
   unsigned char buff[EVP_MAX_MD_SIZE];
   unsigned int buff_len;
+
   EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+
   printf("Enter your value to be encrypted: ");
   fgets(value, sizeof(value), stdin);
   value[strcspn(value, "\n")] = '\0';
+
   EVP_DigestInit_ex(ctx, EVP_sha256(), NULL);
   EVP_DigestUpdate(ctx, value, strlen(value));
   EVP_DigestFinal(ctx, buff, &buff_len);
+
   for (int i = 0; i < buff_len; i++) {
     printf("%02x", buff[i]);
   }
